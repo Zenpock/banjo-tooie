a083bf03b81adbb05b6e467ccb83661104f896fb
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-02-27 13:35:58 -0600

    Ported string funcs and func_800BE600

--- include/core2/1E97EF0.h
+++ include/core2/1E97EF0.h
@@ -2,7 +2,8 @@
 #define __CORE2_1E97EF0_H__
 
 #include "common.h"
+#include "core2/1EB5980.h"
 
-s32 func_800BEF00(f32[3], f32[3], f32[3], s32);
+void* func_800BEF00(f32[3], f32[3], f32[3], s32);
 
 #endif
