4dd04b30b3fbfb6ce6a750b72316ef60e436e805
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-03-06 17:06:37 -0600

    match ch golden egg

--- include/ch/egg/golden.h
+++ include/ch/egg/golden.h
@@ -2,7 +2,18 @@
 #define __CH_EGG_GOLDEN_H__
 
 #include "common.h"
-
+#include "fx/egg.h"
+#include "su/egg.h"
+#include "su/eggmisc.h"
+#include "su/eggmove.h"
+#include "core2/1E82660.h"
+#include "core2/1E9A960.h"
+#include "core2/1EB2840.h"
+#include "core2/1EB5980.h"
+#include "core2/1EC8070.h"
+#include "core2/1ECA640.h"
+#include "core2/1ECE0B0.h"
+#include "core2/1ED8C80.h"
 
 
 #endif
