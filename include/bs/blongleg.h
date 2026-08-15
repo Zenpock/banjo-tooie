dc4dfb9caa54b3755dadade591ce7fdaf4aefc43
CyrusKashef <cyrus.kashef3@outlook.com>
2026-08-15 10:29:25 -0500

    Completed BLongLeg

--- include/bs/blongleg.h
+++ include/bs/blongleg.h
@@ -7,14 +7,13 @@
 #include "overlays/ba/alarm.h"
 #include "overlays/ba/anim.h"
 #include "overlays/ba/flag.h"
-// #include "overlays/ba/key.h"
+#include "overlays/ba/input.h"
 #include "overlays/ba/physics.h"
 #include "overlays/ba/playerstate.h"
 #include "overlays/ba/roll.h"
 #include "overlays/ba/statetimer.h"
 #include "overlays/ba/stick.h"
 #include "overlays/ba/timer.h"
-// #include "overlays/ba/yaw.h"
 
 #include "overlays/bs/state.h"
 
@@ -23,52 +22,53 @@
 #include "core2/1E75710.h"
 #include "core2/1E76880.h"
 #include "core2/1E77A20.h"
+#include "core2/1E78BF0.h"
 #include "core2/1E79FD0.h"
 #include "core2/1E76CC0.h"
 #include "core2/1EA0AA0.h"
 #include "core2/1ECA640.h"
 
 s32 func_80800000_bsblongleg(PlayerState*);
-// func_80800020_bsblongleg
-// func_80800064_bsblongleg
+void func_80800020_bsblongleg(PlayerState*, s32);
+void func_80800064_bsblongleg(PlayerState*);
 void func_808000C8_bsblongleg(PlayerState*);
-// func_8080013C_bsblongleg
+void func_8080013C_bsblongleg(PlayerState*);
 void func_80800204_bsblongleg(PlayerState*);
 void func_808002D8_bsblongleg(PlayerState*);
-// func_80800378_bsblongleg
-// bsblongleg_entrypoint_0
-// bsblongleg_entrypoint_1
-// bsblongleg_entrypoint_2
-// bsblongleg_entrypoint_3
-// bsblongleg_entrypoint_4
-// bsblongleg_entrypoint_5
+void func_80800378_bsblongleg(PlayerState*);
+void bsblongleg_entrypoint_0(PlayerState*);
+void bsblongleg_entrypoint_1(PlayerState*);
+void bsblongleg_entrypoint_2(PlayerState*);
+s32 bsblongleg_entrypoint_3(s32);
+void bsblongleg_entrypoint_4(PlayerState*);
+void bsblongleg_entrypoint_5(PlayerState*);
 void bsblongleg_entrypoint_6(PlayerState*);
-// bsblongleg_entrypoint_7
-// bsblongleg_entrypoint_8
-// bsblongleg_entrypoint_9
+s32 bsblongleg_entrypoint_7(s32);
+void bsblongleg_entrypoint_8(PlayerState*);
+void bsblongleg_entrypoint_9(PlayerState*);
 void bsblongleg_entrypoint_10(PlayerState*);
-// bsblongleg_entrypoint_11
-// func_80800994_bsblongleg
-// func_80800A18_bsblongleg
-// bsblongleg_entrypoint_12
-// bsblongleg_entrypoint_13
-// bsblongleg_entrypoint_14
-// bsblongleg_entrypoint_15
-// bsblongleg_entrypoint_16
-// bsblongleg_entrypoint_17
+s32 bsblongleg_entrypoint_11(s32);
+void func_80800994_bsblongleg(PlayerState*);
+void func_80800A18_bsblongleg(PlayerState*);
+void bsblongleg_entrypoint_12(PlayerState*);
+void bsblongleg_entrypoint_13(PlayerState*);
+void bsblongleg_entrypoint_14(PlayerState*);
+s32 bsblongleg_entrypoint_15(s32);
+void bsblongleg_entrypoint_16(PlayerState*);
+void bsblongleg_entrypoint_17(PlayerState*);
 void bsblongleg_entrypoint_18(PlayerState*);
-// bsblongleg_entrypoint_19
-// bsblongleg_entrypoint_20
-// bsblongleg_entrypoint_21
+s32 bsblongleg_entrypoint_19(s32);
+void bsblongleg_entrypoint_20(PlayerState*);
+void bsblongleg_entrypoint_21(PlayerState*);
 void bsblongleg_entrypoint_22(PlayerState*);
-// bsblongleg_entrypoint_23
+s32 bsblongleg_entrypoint_23(s32);
 void bsblongleg_entrypoint_24(PlayerState*);
 void bsblongleg_entrypoint_25(PlayerState*);
-// bsblongleg_entrypoint_26
-// bsblongleg_entrypoint_27
+void bsblongleg_entrypoint_26(PlayerState*);
+s32 bsblongleg_entrypoint_27(s32);
 void bsblongleg_entrypoint_28(PlayerState*);
-// bsblongleg_entrypoint_29
+void bsblongleg_entrypoint_29(PlayerState*);
 void bsblongleg_entrypoint_30(PlayerState*);
-// bsblongleg_entrypoint_31
+s32 bsblongleg_entrypoint_31(s32);
 
 #endif
