479942bb4b8a4dfd8cc014e1982a97f75df29268
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-08-19 10:15:19 -0500

    Match fxdialog

--- include/core2/1EBB4F0.h
+++ include/core2/1EBB4F0.h
@@ -4,8 +4,10 @@
 #include "common.h"
 #include "core2/1EAF950.h"
 
-void func_800E2588(u32);
+void func_800E2554(f32, f32);
 void func_800E2568(s32, s32);
+void func_800E257C(s32);
+void func_800E2588(u32);
 void func_800E2594(u32, ImageStruct *);
 void func_800E30E0(s32, void*, s32, void*);
 
--- include/fx/dialog.h
+++ include/fx/dialog.h
@@ -4,7 +4,10 @@
 #include "common.h"
 #include "core2/1EAF950.h"
 #include "core2/1EB5E70.h"
+#include "core2/1EB93C0.h"
+#include "core2/1EBB4F0.h"
 #include "core2/1EC8070.h"
+#include "core2/1EDD9C0.h"
 
 void _fxdialog_entrypoint_1(s32 arg0, s32 arg1);
 void _fxdialog_entrypoint_2(f32);
