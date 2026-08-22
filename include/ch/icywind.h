95d69c5de83750b9c707821b57d7fb065ff56650
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-08-21 21:33:12 -0500

    match icy wind and dinomother

---
+++ include/ch/icywind.h
@@ -0,0 +1,14 @@
+#ifndef __CH_ICYWIND_H__
+#define __CH_ICYWIND_H__
+
+#include "common.h"
+#include "core2/1E93440.h"
+#include "core2/1EA9160.h"
+#include "core2/1ECE0B0.h"
+#include "core2/1ED8C80.h"
+#include "core2/1EE5DF0.h"
+#include "su/baddieaudioloop.h"
+
+
+
+#endif
