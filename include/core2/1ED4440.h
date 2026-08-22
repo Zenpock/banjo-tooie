2a5d69eb61d327ff2e7f704f2d06316ba7d71750
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-08-21 21:36:04 -0500

    match dippy, poolkids, fxairbub,fxdialog,fxripple

---
+++ include/core2/1ED4440.h
@@ -0,0 +1,9 @@
+#ifndef __CORE2_1ED4440_H__
+#define __CORE2_1ED4440_H__
+
+#include "common.h"
+
+s32 func_800FB2AC(f32*, f32);
+s32 func_800FB338(f32*, s32, s32*, f32*);
+
+#endif
--- include/fx/dialog.h
+++ include/fx/dialog.h
@@ -4,8 +4,12 @@
 #include "common.h"
 #include "core2/1EAF950.h"
 #include "core2/1EB5E70.h"
+#include "core2/1EB93C0.h"
+#include "core2/1EBB4F0.h"
 #include "core2/1EC8070.h"
+#include "core2/1EDD9C0.h"
 
+void _fxdialog_entrypoint_0(void);
 void _fxdialog_entrypoint_1(s32 arg0, s32 arg1);
 void _fxdialog_entrypoint_2(f32);
 void _fxdialog_entrypoint_3(void*, f32, u8, s8);
