0c8968316013dedd06fe6542642f9d8bdc7efcb9
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-09-16 15:19:10 -0500

    Match bsbanshack

--- include/bs/ban/shack.h
+++ include/bs/ban/shack.h
@@ -2,43 +2,61 @@
 #define __BS_BAN_SHACK__
 
 
-#include <ultra64.h>
 #include "common.h"
+#include "ba/anim.h"
+#include "ba/backpack.h"
+#include "ba/input.h"
+#include "ba/key.h"
+#include "ba/physics.h"
+#include "ba/playerstate.h"
+#include "ba/stick.h"
+#include "bs/ban/shack.h"
+#include "bs/mum/move.h"
+#include "bs/mum/still.h"
+#include "bs/rest.h"
+#include "bs/state.h"
+#include "core2/1E65B90.h"
+#include "core2/1E66990.h"
+#include "core2/1E6F080.h"
+#include "core2/1E76CC0.h"
+#include "core2/1E77A20.h"
+#include "core2/1E78BF0.h"
+#include "core2/1E79FD0.h"
+#include "core2/1E7BFA0.h"
+#include "core2/1EB5980.h"
+#include "core2/1ECA640.h"
+#include "core2/anctrl.h"
+#include "player.h"
+#include "ba/anim.h"
+#include "ba/backpack.h"
+#include "ba/input.h"
+#include "ba/key.h"
+#include "ba/physics.h"
+#include "ba/stick.h"
+#include "bs/ban/shack.h"
+#include "bs/mum/move.h"
+#include "bs/mum/still.h"
+#include "bs/rest.h"
+#include "bs/state.h"
+#include "core2/1E65B90.h"
+#include "core2/1E66990.h"
+#include "core2/1E6F080.h"
+#include "core2/1E76CC0.h"
+#include "core2/1E77A20.h"
+#include "core2/1E78BF0.h"
+#include "core2/1E79FD0.h"
+#include "core2/1E7BFA0.h"
+#include "core2/1EB5980.h"
+#include "core2/1ECA640.h"
+#include "core2/anctrl.h"
 #include "player.h"
 
-// func_80800000_bsbanshack
-// func_80800074_bsbanshack
-// func_80800100_bsbanshack
-// func_80800164_bsbanshack
-// func_8080021C_bsbanshack
-// func_808002AC_bsbanshack
-// func_8080030C_bsbanshack
-// func_8080037C_bsbanshack
-// bsbanshack_entrypoint_0
-void func_80800430_bsbanshack();
-// func_80800450_bsbanshack
-// func_808004AC_bsbanshack
-// bsbanshack_entrypoint_1
-// func_808005DC_bsbanshack
-void func_80800744_bsbanshack(s32 arg0);
-// func_80800770_bsbanshack
-// func_808007A0_bsbanshack
-// bsbanshack_entrypoint_2
-void func_80800928_bsbanshack();
-// func_80800948_bsbanshack
-// func_808009E4_bsbanshack
-// bsbanshack_entrypoint_3
-void func_80800AD0_bsbanshack(s32 arg0);
-void func_80800AF8_bsbanshack(s32 arg0);
-void func_80800B20_bsbanshack(PlayerState* self);
-// bsbanshack_entrypoint_4
-void func_80800B54_bsbanshack(s32 arg0);
-void func_80800B7C_bsbanshack(s32 arg0);
-// func_80800BA4_bsbanshack
-// bsbanshack_entrypoint_5
-void func_80800BF0_bsbanshack();
-// func_80800C10_bsbanshack
-// func_80800C6C_bsbanshack
-// bsbanshack_entrypoint_6
+s32 _bsbanshack_entrypoint_0(s32 arg0);
+s32 _bsbanshack_entrypoint_1(s32 arg0);
+s32 _bsbanshack_entrypoint_2(s32 arg0);
+s32 _bsbanshack_entrypoint_3(s32 arg0);
+s32 _bsbanshack_entrypoint_4(s32 arg0);
+s32 _bsbanshack_entrypoint_5(s32 arg0);
+s32 _bsbanshack_entrypoint_6(s32 arg0);
 
 #endif // __BS_BAN_SHACK__
