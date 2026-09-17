06aa7f9c4e50fd370b2a6d3237b40d911aa65475
Zenpock <70054140+Zenpock@users.noreply.github.com>
2026-09-16 20:30:38 -0500

    Match bsbanswim

--- include/bs/ban/swim.h
+++ include/bs/ban/swim.h
@@ -2,30 +2,31 @@
 #define __BS_BAN_SWIM__
 
 
-#include <ultra64.h>
-#include "common.h"
 
-// func_80800000_bsbanswim
-// func_80800030_bsbanswim
-// func_80800104_bsbanswim
-void func_808001A8_bsbanswim();
-// func_808001C8_bsbanswim
-// func_8080025C_bsbanswim
-// bsbanswim_entrypoint_0
-// func_80800390_bsbanswim
-// func_808003BC_bsbanswim
-// func_8080040C_bsbanswim
-void func_8080049C_bsbanswim();
-// func_808004BC_bsbanswim
-// func_808004F4_bsbanswim
-// bsbanswim_entrypoint_1
-void func_80800670_bsbanswim();
-// func_80800690_bsbanswim
-// func_808006CC_bsbanswim
-// bsbanswim_entrypoint_2
-void func_80800848_bsbanswim();
-// func_80800868_bsbanswim
-// func_808008A4_bsbanswim
-// bsbanswim_entrypoint_3
+#include "common.h"
+#include "ba/airbar.h"
+#include "ba/anim.h"
+#include "ba/packctrl.h"
+#include "ba/physics.h"
+#include "ba/playerstate.h"
+#include "ba/roll.h"
+#include "ba/yaw.h"
+#include "ba/swim.h"
+#include "bs/ban/swim.h"
+#include "bs/state.h"
+#include "core2/1E66990.h"
+#include "core2/1E6B900.h"
+#include "core2/1E6F080.h"
+#include "core2/1E75710.h"
+#include "core2/1E76CC0.h"
+#include "core2/1E77A20.h"
+#include "core2/1E78BF0.h"
+#include "core2/1E79FD0.h"
+#include "core2/1EA0690.h"
+#include "core2/1ECE0B0.h"
+#include "core2/anctrl.h"
+#include "player.h"
+
+
 
 #endif // __BS_BAN_SWIM__
--- src/overlays/bs/ban/swim.c
+++ src/overlays/bs/ban/swim.c
@@ -1,57 +1,340 @@
 #include "bs/ban/swim.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_80800000_bsbanswim.s")
+extern s32 D_80800A30_bsbanswim[];
+extern s32 D_80800A40_bsbanswim[];
+extern s32 D_80800A50_bsbanswim[];
+extern s32 D_80800A60_bsbanswim[];
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_80800030_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_80800104_bsbanswim.s")
-
-void func_808001A8_bsbanswim()
+void func_80800000_bsbanswim(PlayerState* self)
 {
-    func_80800104_bsbanswim();
+    func_8009FFD8(self, BAANIM_UPDATE_1_NORMAL, YAW_TYPE_3_BOUNDED, 3, BA_PHYSICS_9_NO_GRAVITY);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_808001C8_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_8080025C_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/bsbanswim_entrypoint_0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_80800390_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_808003BC_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_8080040C_bsbanswim.s")
-
-void func_8080049C_bsbanswim()
+void func_80800030_bsbanswim(PlayerState* self)
 {
-    func_808003BC_bsbanswim();
+    if (func_8009E77C(self, 0xB) == 0)
+    {
+        _baswim_entrypoint_5(self);
+        baroll_setAngularVelocity(self, 130.0f, 0.9f);
+        baphysics_set_gravity(self, 0.0f);
+        baphysics_set_terminal_velocity(self, -399.99f);
+        if (func_800F8B88() != 3)
+        {
+            func_800A0CF4(self, 1);
+        }
+        func_800961AC(self, 3);
+        func_80093360(self, 60.0f);
+        func_8009BC5C(self, 2.0f);
+        _baswim_entrypoint_6(self, 1);
+        if (bs_getCurrentState(self) != BS_STATE_4C_LANDING_IN_WATER)
+        {
+            _baairbar_entrypoint_0(self, 2);
+        }
+    }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_808004BC_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_808004F4_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/bsbanswim_entrypoint_1.s")
-
-void func_80800670_bsbanswim()
+void func_80800104_bsbanswim(PlayerState* self) {
+    if (func_8009E74C(self, 0xB) == 0) {
+        func_8009BF5C(self, 0.0f);
+        baroll_setIdeal(self, 0.0f);
+        func_8009BC34(self);
+        func_800A0CF4(self, 0);
+        func_800961AC(self, 1);
+        func_80093360(self, 0.0f);
+        _baswim_entrypoint_6(self, 0);
+        if (bs_getCurrentState(self) != BS_STATE_4C_LANDING_IN_WATER) {
+            _baairbar_entrypoint_0(self, 1);
+        }
+        _baswim_entrypoint_4(self);
+    }
+}
+void func_808001A8_bsbanswim(PlayerState* self)
 {
-    func_808003BC_bsbanswim();
+    func_80800104_bsbanswim(self);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_80800690_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_808006CC_bsbanswim.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/bsbanswim_entrypoint_2.s")
-
-void func_80800848_bsbanswim()
-{
-    func_808003BC_bsbanswim();
+void func_808001C8_bsbanswim(PlayerState* self) {
+    func_800C6DA0(0x3F);
+    baanim_playForDuration_onceSmoothStartingAt(self, 0x27D, 1.7f, 0.1f);
+    func_80800000_bsbanswim(self);
+    yaw_setVelocityBounded(self, 500.0f, 5.0f);
+    func_80800030_bsbanswim(self);
+    _baswim_entrypoint_10(self, 0.0f);
+    _baswim_entrypoint_8(self, 600.0f);
+    func_8009BFE4(self, 200.0f, 2.5f);
+    self->unk164.bytes[0] = 0;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_80800868_bsbanswim.s")
+void func_8080025C_bsbanswim(PlayerState* self) {
+    BanjoStateId sp24;
+    AnimCtrl* sp20;
+    u8 temp_v1;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/func_808008A4_bsbanswim.s")
+    sp24 = BS_STATE_0_INVALID;
+    sp20 = baanim_getAnimCtrlPtr(self);
+    temp_v1 = self->unk164.bytes[0];
+    switch (temp_v1) {                              /* irregular */
+    case 0:
+        func_8009BF5C(self, 50.0f);
+        if (anctrl_isAt(sp20, 0.37f) != 0) {
+            if (_baswim_entrypoint_1(self) != 0) {
+                func_800A0E64(self);
+            }
+            _baswim_entrypoint_10(self, 300.0f);
+            _baswim_entrypoint_8(self, 200.0f);
+            self->unk164.bytes[0] = 1;
+        }
+        break;
+    case 1:
+        _baswim_entrypoint_11(self);
+        _baswim_entrypoint_13(self);
+        if (anctrl_isAt(sp20, 0.76f) != 0) {
+            sp24 = 0x2B;
+        }
+        break;
+    }
+    if (anctrl_isAt(sp20, 0.6f) != 0) {
+        func_8009DE38(self, 0x3FD, 1.0f);
+    }
+    if (player_inWater(self) == 0) {
+        sp24 = BS_STATE_1_IDLE;
+    }
+    bs_setState(self, sp24);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/bs/ban/swim/bsbanswim_entrypoint_3.s")
+s32 bsbanswim_entrypoint_0(s32 arg0) {
+    return D_80800A30_bsbanswim[arg0];
+}
+
+s32 func_80800390_bsbanswim(BanjoStateId arg0)
+{
+    if ((arg0 == 0x81) || (arg0 == 0x82) || (arg0 == 0x90))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void func_808003BC_bsbanswim(PlayerState* self)
+{
+    if (func_80800390_bsbanswim(bs_getNextState(self)) == 0)
+    {
+        if (func_8008E0E8(self) != 0) {
+            _bapackctrl_entrypoint_5(self, 3);
+        }
+        func_80800104_bsbanswim(self);
+    }
+}
+
+void func_8080040C_bsbanswim(PlayerState* self)
+{
+    if (func_80800390_bsbanswim(bs_getPreviousState(self)) == 0)
+    {
+        baanim_playForDuration_loopSmooth(self, 0x27E, 0.75f);
+        func_80800000_bsbanswim(self);
+        func_80800030_bsbanswim(self);
+        _baswim_entrypoint_10(self, 0.0f);
+        _baswim_entrypoint_8(self, 600.0f);
+        func_8009BC5C(self, 2.0f);
+        if (func_8008E0E8(self) != 0)
+        {
+            _bapackctrl_entrypoint_5(self, 4);
+        }
+    }
+}
+
+void func_8080049C_bsbanswim(PlayerState* self)
+{
+    func_808003BC_bsbanswim(self);
+}
+
+void func_808004BC_bsbanswim(PlayerState* self) {
+    AnimCtrl* sp1C;
+
+    sp1C = baanim_getAnimCtrlPtr(self);
+    func_8080040C_bsbanswim(self);
+    anctrl_setDuration(sp1C, 1.0f);
+}
+
+void func_808004F4_bsbanswim(PlayerState* self) {
+    BanjoStateId sp24;
+    AnimCtrl* sp20;
+
+    sp24 = BS_STATE_0_INVALID;
+    sp20 = baanim_getAnimCtrlPtr(self);
+    _baswim_entrypoint_11(self);
+    _baswim_entrypoint_12(self);
+    _baswim_entrypoint_13(self);
+    if (anctrl_isAt(sp20, 0.512f) != 0) {
+        _baswim_entrypoint_10(self, 320.0f);
+    }
+    else {
+        _baswim_entrypoint_10(self, 250.0f);
+    }
+    if (anctrl_isAt(sp20, 0.17f) != 0) {
+        func_8009DF18(self, 0x3FD, 1.9f, 0x2710);
+    }
+    if (anctrl_isAt(sp20, 0.67f) != 0) {
+        func_8009DF18(self, 0x3FD, 1.7f, 0x2710);
+    }
+    switch (_baswim_entrypoint_3(self)) {
+    case 1:
+        break;
+    case 0:
+        if (anctrl_isAt(sp20, 0.99f) != 0) {
+            sp24 = 0x2B;
+        }
+        break;
+    case 2:
+        sp24 = 0x82;
+        break;
+    case 3:
+        sp24 = 0x90;
+        break;
+    }
+    if (_baswim_entrypoint_2(self) != 0) {
+        sp24 = 0x2D;
+    }
+    if (player_inWater(self) == 0) {
+        sp24 = BS_STATE_1_IDLE;
+    }
+    bs_setState(self, sp24);
+}
+
+s32 bsbanswim_entrypoint_1(s32 arg0) {
+    return D_80800A40_bsbanswim[arg0];
+}
+
+void func_80800670_bsbanswim(PlayerState* self)
+{
+    func_808003BC_bsbanswim(self);
+}
+
+void func_80800690_bsbanswim(PlayerState* self) {
+    AnimCtrl* sp1C;
+
+    sp1C = baanim_getAnimCtrlPtr(self);
+    func_8080040C_bsbanswim(self);
+    anctrl_setDuration(sp1C, 0.8f);
+}
+
+void func_808006CC_bsbanswim(PlayerState* self) {
+    BanjoStateId sp24;
+    AnimCtrl* sp20;
+    s32 temp_v0;
+
+    sp24 = BS_STATE_0_INVALID;
+    sp20 = baanim_getAnimCtrlPtr(self);
+    _baswim_entrypoint_11(self);
+    _baswim_entrypoint_12(self);
+    _baswim_entrypoint_13(self);
+    if (anctrl_isAt(sp20, 0.512f) != 0) {
+        _baswim_entrypoint_10(self, 450.0f);
+    }
+    else {
+        _baswim_entrypoint_10(self, 300.0f);
+    }
+    if (anctrl_isAt(sp20, 0.17f) != 0) {
+        func_8009DF18(self, 0x3FD, 1.9f, 0x2710);
+    }
+    if (anctrl_isAt(sp20, 0.67f) != 0) {
+        func_8009DF18(self, 0x3FD, 1.7f, 0x2710);
+    }
+    temp_v0 = _baswim_entrypoint_3(self);
+    switch (temp_v0) {                              /* irregular */
+    case 2:
+        break;
+    case 0:
+        if (anctrl_isAt(sp20, 0.99f) != 0) {
+            sp24 = 0x2B;
+        }
+        break;
+    case 1:
+        sp24 = 0x81;
+        break;
+    case 3:
+        sp24 = 0x90;
+        break;
+    }
+    if (_baswim_entrypoint_2(self) != 0) {
+        sp24 = 0x2D;
+    }
+    if (player_inWater(self) == 0) {
+        sp24 = BS_STATE_1_IDLE;
+    }
+    bs_setState(self, sp24);
+}
+
+s32 bsbanswim_entrypoint_2(s32 arg0) {
+    return D_80800A50_bsbanswim[arg0];
+}
+void func_80800848_bsbanswim(PlayerState* self)
+{
+    func_808003BC_bsbanswim(self);
+}
+
+void func_80800868_bsbanswim(PlayerState* self) 
+{
+    AnimCtrl* sp1C;
+
+    sp1C = baanim_getAnimCtrlPtr(self);
+    func_8080040C_bsbanswim(self);
+    anctrl_setDuration(sp1C, 0.7f);
+}
+
+void func_808008A4_bsbanswim(PlayerState* self)
+{
+    BanjoStateId sp24;
+    AnimCtrl* sp20;
+    sp24 = BS_STATE_0_INVALID;
+    sp20 = baanim_getAnimCtrlPtr(self);
+    _baswim_entrypoint_11(self);
+    _baswim_entrypoint_12(self);
+    _baswim_entrypoint_13(self);
+    if (anctrl_isAt(sp20, 0.512f) != 0)
+    {
+        _baswim_entrypoint_10(self, 600.0f);
+    }
+    else
+    {
+        _baswim_entrypoint_10(self, 400.0f);
+    }
+    if (anctrl_isAt(sp20, 0.17f) != 0)
+    {
+        func_8009DF18(self, 0x3FD, 1.9f, 0x2710);
+    }
+    if (anctrl_isAt(sp20, 0.67f) != 0)
+    {
+        func_8009DF18(self, 0x3FD, 1.7f, 0x2710);
+    }
+    switch (_baswim_entrypoint_3(self))
+    {
+    case 3:
+        break;
+    case 0:
+        if (anctrl_isAt(sp20, 0.99f) != 0)
+        {
+            sp24 = 0x2B;
+        }
+        break;
+    case 1:
+        sp24 = 0x81;
+        break;
+    case 2:
+        sp24 = 0x82;
+        break;
+    }
+    if (_baswim_entrypoint_2(self) != 0)
+    {
+        sp24 = 0x2D;
+    }
+    if (player_inWater(self) == 0)
+    {
+        sp24 = BS_STATE_1_IDLE;
+    }
+    bs_setState(self, sp24);
+}
+
+s32 bsbanswim_entrypoint_3(s32 arg0) {
+    return D_80800A60_bsbanswim[arg0];
+}
